364068b8b20ff81669b501932dc95a5fee048a8b
Lahaye <Nicholas.J.LaHaye@jpl.nasa.gov>
2020-05-28 12:26:42 -0700

    Adding configuration-based input

--- misr_modis_fuse_dbn_standard_9cam_learnergy.py
+++ misr_modis_fuse_dbn_standard_9cam_learnergy.py
@@ -1,4 +1,4 @@
-
+#General Imports
 import os
 import numpy as np
 import random
@@ -8,26 +8,63 @@ sys.setrecursionlimit(4500)
 
 #ML imports
 import torch
-from learnergy.core.dataset import Dataset
 from learnergy.models import dbn
-from sklearn.preprocessing import StandardScaler
  
 #Data
 from MISR_MODIS_FUSE_DATA_FIREX_9CAM_9 import data_fn3, data_fn3_test, NUMBER_CHANNELS, CHUNK_SIZE
 from dbnDatasets import DBNDataset
-from utils import numpy_to_torch
+from utils import numpy_to_torch, read_yaml, get_read_func
 
+#Input Parsing
+import pyyaml
+import argparse
 
-def test_misr(chunk_size, number_channel, data_train, data_test, out_dir, use_gpu = True):
- 
+def run_dbn(yml_conf, data_train, data_test):
 
+    #Get config values 
+    chunk_size = yml_conf["data"]["chunk_size"]
+    number_channel = yml_conf["data"]["number_channel"]
+    data_reader =  yml_conf["data"]["reader_type"]
+    fill = yml_conf["data"]["fill_value"]
+    chan_dim = yml_conf["data"]["chan_dim"]
+    valid_min = yml_conf["data"]["valid_min"]
+    valid_max = yml_conf["data"]["valid_max"]
+    delete_chans = yml_conf["data"]["delete_chans"]
+
+    out_dir = yml_conf["output"]["out_dir"]
+    os.makedirs(out_dir, exist_ok=True)
+
+    model_fname = yml_conf["output"]["model"]
+    training_output = yml_conf["output"]["training_output"]
+    training_mse = yml_conf["output"]["training_mse"]
+    testing_output = yml_conf["output"]["testing_output"]
+    testing_mse = yml_conf["output"]["testing_mse"]
+
+    model_type = yml_conf["dbn"]["params"]["model_type"]
+    dbn_arch = yml_conf["dbn"]["params"]["dbn_arch"]
+    gibbs_steps = yml_conf["dbn"]["params"]["gibbs_steps"]
+    learning_rate = yml_conf["dbn"]["params"]["learning_rate"]
+    momentum = yml_conf["dbn"]["params"]["momentum"]
+    decay = yml_conf["dbn"]["params"]["decay"]
+    temp = yml_conf["dbn"]["params"]["temp"]
+    
+    use_gpu = yml_conf["dbn"]["training"]["use_gpu"]
+    batch_size = yml_conf["dbn"]["training"]["batch_size"]
+    epochs = yml_conf["dbn"]["training"]["epochs"]
+
+    read_func = get_read_func(data_reader)
+
+    #Generate training dataset object
     #Unsupervised, so targets are not used. Currently, I use this to store original image indices for each point 
-    x2 = DBNDataset(data_train, np.load, chunk_size, delete_chans=[66], valid_min=0, valid_max=None, fill_value = -9999, chan_dim = 0, scalers = None, transform=numpy_to_torch)
+    x2 = DBNDataset(data_train, read_func, chunk_size, delete_chans=delete_chans, valid_min=valid_min, valid_max=valid_max, fill_value =fill, chan_dim = chan_dim, scalers = None, transform=numpy_to_torch)
 
-    new_dbn = dbn.DBN(model='variance_gaussian', n_visible=chunk_size*chunk_size*number_channel, n_hidden=[2000], steps=[10], learning_rate=[0.01], momentum=[0.95], decay=[0.0001], temperature=[0.5], use_gpu=use_gpu)
-    new_dbn.fit(x2, batch_size=10, epochs=[1])	
+    #Generate model
+    new_dbn = dbn.DBN(model=model_type, n_visible=chunk_size*chunk_size*number_channel, n_hidden=dbn_arch, steps=gibbs_steps, learning_rate=learning_rate, momentum=momentum, decay=decay, temperature=temp, use_gpu=use_gpu)
 
-  
+    #Train model
+    new_dbn.fit(x2, batch_size=batch_size, epochs=epochs)	
+
+    #Generate training data output 
     if torch.cuda.is_available() and use_gpu:	
         output = new_dbn.forward(x2.data.cuda()).cpu()
         x2.transform = None
@@ -36,13 +73,15 @@ def test_misr(chunk_size, number_channel, data_train, data_test, out_dir, use_gp
         output = new_dbn.forward(x2.data)
         x2.transform = None
         rec_mse, v = new_dbn.reconstruct(x2) 
-  
-    torch.save(output, out_dir + "/output.data")
-    torch.save(rec_mse, out_dir + "/rec_mse.data")
+   
+    #Save training output
+    torch.save(output, os.path.join(out_dir, training_output))
+    torch.save(rec_mse, os.path.join(out_dir, training_mse))
 
-
-    x3 = DBNDataset(data_test, np.load, chunk_size, delete_chans=[66], valid_min=0, valid_max=None, fill_value = -9999, chan_dim = 0, scalers=x2.scalers, transform=numpy_to_torch)
+    #Generate test dataset object
+    x3 = DBNDataset(data_test, read_func, chunk_size, delete_chans=delete_chans, valid_min=valid_min, valid_max=valid_max, fill_value = fill, chan_dim = chan_dim, scalers=x2.scalers, transform=numpy_to_torch)
  
+    #Generate test data output
     if torch.cuda.is_available() and use_gpu:
         output = new_dbn.forward(x3.data.cuda()).cpu()
         x3.transform = None
@@ -52,20 +91,28 @@ def test_misr(chunk_size, number_channel, data_train, data_test, out_dir, use_gp
         x3.transform = None
         rec_mse, v = new_dbn.reconstruct(x3) 
    
-    torch.save(output, out_dir + "/output_test.data")
-    torch.save(rec_mse, out_dir + "/rec_mse_test.data")
+    #Save test data output
+    torch.save(output, os.path.join(out_dir, testing_output))
+    torch.save(rec_mse, os.path.join(out_dir, testing_mse))
  
-    torch.save(new_dbn, out_dir + "/dbn.pth")
+    #Save model
+    torch.save(new_dbn, os.path.join(out_dir, model_fname))
     
 
-def main(chunk_size, number_channel, data_train, data_test, out_dir):
-    test_misr(chunk_size, number_channel, data_train, data_test, out_dir)
+def main(yml_fpath, data_train, data_test):
+   
+    #Translate config to dictionary 
+    yml_conf = read_yaml(yaml_fpath)
+    #Run 
+    test_misr(yml_conf, data_train, data_test)
 
 
 if __name__ == '__main__':
-    out_dir = 'output/Learnergy/DBN_MISR_MODIS_1/'
-    os.makedirs(out_dir, exist_ok=True)
-    main(CHUNK_SIZE, NUMBER_CHANNELS, data_fn3, data_fn3_test, out_dir)
+	
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-y", "--yaml", help="YAML file for DBN and output config.")
+    args = parser.parse_args()
+    main(args.yaml, data_fn3, data_fn3_test)
 
 
 
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 def torch_to_numpy(trch):
         return trch.numpy()
@@ -6,4 +7,17 @@ def torch_to_numpy(trch):
 def numpy_to_torch(npy):
         return torch.from_numpy(npy)
 
+
+def read_yaml(fpath_yaml):
+    yml_conf = None
+    with open(path_yaml) as f_yaml:
+        yml_conf = yaml.load(f_yaml)
+    return yml_conf
+
+
+def get_read_func(data_reader):
+	if data_reader == "numpy"
+		return np.load
+	#TODO return BCDP reader
+	return None
 
