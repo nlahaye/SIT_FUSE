6ba6577d6c11994fae3413169c8f307aa8b8787c
nikleet <nikvleet@gmail.com>
2023-08-22 17:14:45 -0500

    Fixing bugs and added dbn_run.py

--- dbn_run.py
+++ dbn_run.py
@@ -1,50 +1,135 @@
 # General imports
 import os
-import glob
+from glob import glob
+import numpy as np
 
 # Module imports
-import dbn_learnergy
+import torch
+# import dbn_learnergy
 import discretize_clusters
-import dbn_learnergy_heirarchical
-import postprocessing.generate_cluster_geotiffs
-import postprocessing.contour_and_fill
+# import dbn_learnergy_heirarchical
+# from postprocessing import generate_cluster_geotiffs
+# from postprocessing import contour_and_fill
 
 # Data
 from utils import read_yaml
+from preprocessing.misc_utils import uavsar_to_geotiff
+import pickle
 
 # Input parsing
 import yaml
 import argparse
 from datetime import timedelta
 
-def main(**kwargs):
+def main(dbn_yaml=None, **kwargs):
     """
+        dbn_yaml: {YAML file for DBN and output config.}
         kwargs:
-            dbn_yaml: {YAML file for DBN and output config.}
             dc_yaml: {YAML file for cluster discretization.}
             
     """
+    dbn_dict = read_yaml(dbn_yaml)
+    out_dir = dbn_dict["output"]["out_dir"]
     
-    dbnyml_conf = read_yaml(kwargs["dbn_yaml"]) 
-    # Translate config to dictionary 
-    out_dir = dbnyml_conf["output"]["out_dir"]
-    os.makedirs(out_dir, exist_ok=True)
-    # Run Initial Feature Extraction & Top Level Clustering
-    dbn_learnergy.run_dbn(dbnyml_conf)
+    # # Translate config to dictionary for dbn  
+    # out_subdir = os.path.join(out_dir, 'run_dbn')
+    # os.makedirs(out_dir, exist_ok=True)
+    # os.makedirs(out_subdir, exist_ok=True)
     
-    data_files = glob(os.path.join(out_dir, "*.data"))
-    clust_dict = {'data': {'filenames': data_files}}
-    print(data_files)
+    # # Run Initial Feature Extraction & Top Level Clustering
+    # dbn_learnergy.run_dbn(dbn_yaml)
+    # data_files = glob(os.path.join(out_dir, "*.data"))
+    
+    # # Create yaml for clustering
+    # clust_dict = {'data': {'filenames': data_files}}
+    # clust_yaml = os.path.join(out_subdir, 'discretize_uavsar_cluster_run_dbn.yml')
+    # with open(clust_yaml, 'w') as outfile:
+    #     yaml.dump(clust_dict, outfile, default_flow_style=True)
+        
+    # # Discretize Model Output - Assign Top Level Labels/Clusters
+    # discretize_clusters.main(clust_yaml)
+    
+    # # # Create Heirarchical Tree/Dendrogram of Labels - Assign More Precise Labels
+    # # dbn_learnergy.run_dbn(dbn_yaml)
+    # # data_files = glob(os.path.join(out_dir, "*.data"))
+    
+    # # # Create geotiffs from testing files
+    # # test_fps = dbn_dict["data"]["files_test"]
+    # # reader_kwargs = dbn_dict["data"]["reader_kwargs"]
+    # # gtiff_data = uavsar_to_geotiff(test_fps, out_subdir, reader_kwargs)
+    
+    # # # Create yaml for intermediate product generation
+    # # clust_reader_type = "uavsar"
+    # # cluster_fnames = glob(os.path.join(out_dir, '*.zarr'))
+    # # subset_inds = []
+    # # for i in enumerate(cluster_fnames):
+    # #     subset_inds.append([])
+    # # create_separate = False
+    # # apply_context = False
+    # # compare_truth = False
+    # # generate_union = False
+    # # gtiff_dict = {'gen_from_geotiffs': False, 
+    # #               'data': 
+    # #                   {'clust_reader_type': clust_reader_type, 
+    # #                    'reader_kwargs': reader_kwargs}, 
+    # #               'subset_inds': subset_inds, 
+    # #               'create_separate': create_separate,
+    # #               'gtiff_data': gtiff_data,
+    # #               'cluster_fnames': cluster_fnames,
+    # #               'context': 
+    # #                   {'apply_context': apply_context,
+    # #                    'clusters': [],
+    # #                    'name': "",
+    # #                    'compare_truth': compare_truth,
+    # #                    'generate_union': generate_union}
+    # #               } 
+    # # gtiff_yaml = os.path.join(out_subdir, 'generate_cluster_geotiffs_run_dbn.yml')
+    # # with open(gtiff_yaml, 'w') as outfile:
+    # #     yaml.dump(gtiff_dict, outfile, default_flow_style=True)
+    
+    # # # GeoTiff/Intermediate Product Generation
+    # # generate_cluster_geotiffs.main(gtiff_yaml)
+    
+    # fp = '/work/09562/nleet/ls6/output/caldor_26200_21048_013_210825_L090HHHH_CX_01.grd.clustoutput_test.data'
+    # file = open(fp, 'rb')
+    # data = pickle.load(file)
+    # print(type(data))
+    # file.close()
+    
+    dat = glob(os.path.join(out_dir, "*.heir_clustoutput*.data"))
+    print(dat)
+    for i in range(len(dat)):
+        if not os.path.exists(dat[i]):
+            continue
+
+        print(dat[i])
+        data = torch.load(dat[i]).numpy()
+        indices = torch.load(dat[i] + ".indices")
+        print(data.shape)
+        print(data.shape[1])
+        print(data[1])
+
+        max_cluster = data.shape[1]
+        min_cluster = 0
+        if data.shape[1] > 1:
+            max_cluster = data.shape[1]
+            disc_data = np.argmax(data, axis = 1)
+            del data
+        else:
+            disc_data = data.astype(np.int32)
+            max_cluster = disc_data.max() #TODO this better!!!
+            del data    
+
+        print(np.unique(disc_data).shape, "UNIQUE LABELS")
     
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-y", "--dbn_yaml", help="YAML file for DBN and output config.")
-    parser.add_argument("-d", "--dc_yaml", nargs='?', help="YAML file for cluster discretization.")
+    parser.add_argument("-y", "--dbn-yaml", help="YAML file for DBN and output config.")
     args = parser.parse_args()
     from timeit import default_timer as timer
     start = timer()
-    main(args)
+    main(args.dbn_yaml)
     end = timer()
     print(end - start) # Time in seconds, e.g. 5.38091952400282
--- preprocessing/misc_utils.py
+++ preprocessing/misc_utils.py
@@ -16,7 +16,6 @@ from scipy.ndimage import uniform_filter
 from scipy.ndimage import variance
 
 from utils import numpy_to_torch, read_yaml, get_read_func, get_lat_lon
-# from utils import read_uavsar, read_annotation
 
 TIF_RE = "(\w+_\w+_)\w+(_\d+_\d+)_wgs84_fit.tif"
 MODIS_BAND_ORDER = ["vis01", "vis02", "vis03", "vis04", "vis05", "vis06", "vis07",  "bt20", "bt21", "bt22", "bt23", "bt24", "bt25", "vis26", "bt27", "bt28", "bt29", "bt30", "bt31", "bt32", "bt33", "bt34", "bt35", "bt36"]
@@ -66,150 +65,107 @@ def lee_filter(img, size):
 
 
 
-# def uavsar_to_geotiff(in_fp, out_dir=None, ann_fp=None, linear_to_dB=False):
-#     """
-#     Converts a UAVSAR file to geotiff.
-#     Args:
-#         in_fp (string): path to input binary file
-#         out_dir (string): directory to save geotiff in
-#         ann_fp (string): path to UAVSAR annotation file
-#         linear_to_dB (bool): convert linear amplitude units to decibels
+def uavsar_to_geotiff(in_fps, out_dir, **kwargs):
+    """
+    Converts UAVSAR file(s) to geotiff.
+    Args:
+        in_fps (list(string) or string):  list of strings (each file will be treated as a separate channel)
+                                          or string of data file paths
+        out_dir (string): directory to which the geotiffs will be saved
+        kwargs:
+            ann_fps (list(string) or string): list of or string of UAVSAR annotation file paths,
+                                            ann files will be automatically matched to data files
 
-#     Returns:
-#         (data, desc, type, out_fp): tuple containing information about the file
-
-#         data: image array of floats or complex values, or dict containing two arrays of slopes (north, east)
-#         desc: the dictionary generated from the annotation file
-#         type: the type of inputted file
-#         out_fp: the file path of the generated tiff
-#     """
-
-#     if not out_dir:
-#         out_dir = os.path.dirname(in_fp)
-#     out_fp = os.path.join(out_dir, os.path.basename(in_fp)) + '.tiff'
-
-#     if os.path.isfile(out_dir):
-#         raise Exception('Provide filepath not the directory.')
-
-#     data = read_uavsar(in_fp, ann_fp, linear_to_dB)
-#     desc = read_annotation(ann_fp)
-
-#     fname = os.path.os.path.basename(in_fp)
-#     exts = fname.split('.')[1:]
-
-#     # Terribly innefficient reuse of code from read_uavsar. Need to rewrite.
+    Returns:
+        data: numpy array of shape (channels, lines, samples) 
+              Complex-valued (unlike polarization) data will be split into separate phase and amplitude channels. 
+    """
     
-#     if len(exts) == 2:
-#         ext = exts[1]
-#         type = exts[0]
-#     elif len(exts) == 1:
-#         type = ext = exts[0]
-#     else:
-#         raise ValueError('Unable to parse extensions')
+    from utils import read_uavsar
+    
+    if "ann_fps" in kwargs:
+        ann_fps = kwargs["ann_fps"]
 
-#     # Check for slant range files and ancillary files
-#     anc = None
-#     if type == 'slope' or type == 'inc':
-#         anc = True
-
-#     if 'start time of acquisition for pass 1' in desc.keys():
-#             mode = 'insar'
-#             raise Exception('INSAR data currently not supported.')
-#     else:
-#         mode = 'polsar'
-
-#     # Determine the correct file typing for searching data dictionary
-#     if not anc:
-#         if mode == 'polsar':
-#             if type == 'hgt':
-#                 search = type
-#             else:
-#                 polarization = os.path.os.path.basename(in_fp).split('_')[5][-4:]
-#                 if polarization == 'HHHH' or polarization == 'HVHV' or polarization == 'VVVV':
-#                         search = f'{type}_pwr'
-#                 else:
-#                     search = f'{type}_phase'
-#                 type = polarization
-
-#         elif mode == 'insar':
-#             if ext == 'grd':
-#                 if type == 'int':
-#                     search = f'grd_phs'
-#                 else:
-#                     search = 'grd'
-#             else:
-#                 if type == 'int':
-#                     search = 'slt_phs'
-#                 else:
-#                     search = 'slt'
-#             pass
-#     else:
-#         search = type
-            
-#     dtype = data.dtype
-#     if dtype is np.complex64:
-#         bands = 2
-#     else:
-#         bands = 1
-
-#     driver = gdal.GetDriverByName("GTiff")
+    if not out_dir:
+        out_dir = os.path.dirname(in_fps)
+    if os.path.isfile(out_dir):
+        raise Exception('Provide a directory, not a filepath.')
+    
+    desc = None
+    type = None
+    search = None
+    
+    data = read_uavsar(in_fps, desc, type, search, kwargs)
+    out_fps = []
+    for dat, fp in zip(data, in_fps):
         
-#     # If ground projected image, north up...
-#     if type in {'grd', 'slope', 'inc'}: 
-#         # Delta latitude and longitude
-#         dlat = float(desc[f'{search}.row_mult']['value'])
-#         dlon = float(desc[f'{search}.col_mult']['value'])
-#         # Upper left corner coordinates
-#         lat1 = float(desc[f'{search}.row_addr']['value'])
-#         lon1 = float(desc[f'{search}.col_addr']['value'])
+        fname = os.path.os.path.basename(fp)
+        out_fp = os.path.join(out_dir, fname) + '.tiff'
+        exts = fname.split('.')[1:]
+        dtype = dat.dtype
+        if dtype == np.complex64:
+            bands = 2
+        else:
+            bands = 1
 
-#         # Set up geotransform for gdal
-#         srs = osr.SpatialReference()
-#         # WGS84 Projection, spatial reference using the EPSG code (4326)
-#         srs.ImportFromEPSG(4326)
-#         t = [lon1, dlon, 0.0, lat1, 0.0, dlat]
+        driver = gdal.GetDriverByName("GTiff")
+            
+        # If ground projected image, north up...
+        if type in {'grd', 'slope', 'inc'}: 
+            # Delta latitude and longitude
+            dlat = float(desc[f'{search}.row_mult']['value'])
+            dlon = float(desc[f'{search}.col_mult']['value'])
+            # Upper left corner coordinates
+            lat1 = float(desc[f'{search}.row_addr']['value'])
+            lon1 = float(desc[f'{search}.col_addr']['value'])
+            # Set up geotransform for gdal
+            srs = osr.SpatialReference()
+            # WGS84 Projection, spatial reference using the EPSG code (4326)
+            srs.ImportFromEPSG(4326)
+            t = [lon1, dlon, 0.0, lat1, 0.0, dlat]
 
-#     if type is 'slope':
-#             slope_fps = []
+        if type == 'slope':
+                out_fps = []
+                for direction, arr in data.items():
+                    slope_fp = out_fp.replace('.tiff',f'.{direction}.tiff')
+                    print(f"saving to {slope_fp}.")
+                    ds = driver.Create(slope_fp, 
+                                        ysize=arr.shape[0], 
+                                        xsize=arr.shape[1], 
+                                        bands=bands, 
+                                        eType=gdal.GDT_Float32)
+                    ds.SetProjection(srs.ExportToWkt())
+                    ds.SetGeoTransform(t)
+                    ds.GetRasterBand(1).WriteArray(np.abs(dat))
+                    ds.GetRasterBand(1).SetNoDataValue(np.nan)
+                    ds.FlushCache()
+                    ds = None
+                    out_fps.append(slope_fp)
+        else:
+            ds = driver.Create(out_fp, 
+                                    ysize=dat.shape[0], 
+                                    xsize=dat.shape[1], 
+                                    bands=bands, 
+                                    eType=gdal.GDT_Float32)
+            if type in {'grd', 'inc'}:
+                ds.SetProjection(srs.ExportToWkt())
+                ds.SetGeoTransform(t)
+            if bands == 2:
+                ds.GetRasterBand(1).WriteArray(np.abs(dat))
+                ds.GetRasterBand(1).SetNoDataValue(np.nan)
+                ds.GetRasterBand(2).WriteArray(np.angle(dat))
+                ds.GetRasterBand(2).SetNoDataValue(np.nan)
+            else:
+                ds.GetRasterBand(1).WriteArray(dat)
+                ds.GetRasterBand(1).SetNoDataValue(np.nan)
+            out_fps.append(out_fp)
             
-#             for direction, arr in data.items():
-#                 slope_fp = out_fp.replace('.tiff',f'.{direction}.tiff')
-#                 print(f"saving to {slope_fp}.")
-#                 ds = driver.Create(slope_fp, 
-#                                     ysize=arr.shape[0], 
-#                                     xsize=arr.shape[1], 
-#                                     bands=bands, 
-#                                     eType=gdal.GDT_Float32)
-#                 ds.SetProjection(srs.ExportToWkt())
-#                 ds.SetGeoTransform(t)
-#                 ds.GetRasterBand(1).WriteArray(np.abs(data))
-#                 ds.GetRasterBand(1).SetNoDataValue(np.nan)
-#                 ds.FlushCache()
-#                 ds = None
-#                 slope_fps.append(slope_fp)
-            
-#             return desc, data, type, slope_fps     
-#     else:
-#         ds = driver.Create(out_fp, 
-#                                 ysize=data.shape[0], 
-#                                 xsize=data.shape[1], 
-#                                 bands=bands, 
-#                                 eType=gdal.GDT_Float32)
-#         if type in {'grd', 'inc'}:
-#             ds.SetProjection(srs.ExportToWkt())
-#             ds.SetGeoTransform(t)
-#         if bands is 2:
-#             ds.GetRasterBand(1).WriteArray(np.abs(data))
-#             ds.GetRasterBand(1).SetNoDataValue(np.nan)
-#             ds.GetRasterBand(2).WriteArray(np.angle(data))
-#             ds.GetRasterBand(2).SetNoDataValue(np.nan)
-#         else:
-#             ds.GetRasterBand(1).WriteArray(data)
-#             ds.GetRasterBand(1).SetNoDataValue(np.nan)
+        ds.FlushCache() # save tiffs
+        ds = None  # close the dataset
     
-#     ds.FlushCache() # save tiffs
-#     ds = None  # close the dataset
-#     return data, desc, type, out_fp
+    print("Saved geotiffs to:")
+    print(out_fps, sep='\n')
+    return out_fps
 
 
 
