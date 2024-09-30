3c0eb4e5177538d38dd2b563f9d6e4d0410c3b74
nlahaye <nlahaye@jpl.nasa.gov>
2024-09-30 17:20:40 +0000

    Updating parameterization

--- src/sit_fuse/preprocessing/rasterize_data.py
+++ src/sit_fuse/preprocessing/rasterize_data.py
@@ -12,14 +12,14 @@ import os
 import argparse
 from sit_fuse.utils import numpy_to_torch, read_yaml, get_read_func
 
-def rasterize(nodata, vector_fn, pixel_size):
+def rasterize(nodata, vector_fn, pixel_size, epsg):
     print(vector_fn)
     source_ex = gdal.OpenEx(vector_fn)
     
     raster_fn = os.path.splitext(vector_fn)[0] + ".tif"
 
 
-    gdal.Rasterize(raster_fn, source_ex, format='GTIFF', outputType=gdal.GDT_Byte, creationOptions=["COMPRESS=DEFLATE"], noData=nodata, initValues=nodata, xRes=pixel_size, yRes=-pixel_size, allTouched=True, burnValues=[1])    
+    gdal.Rasterize(raster_fn, source_ex, format='GTIFF', outputType=gdal.GDT_Byte, creationOptions=["COMPRESS=DEFLATE"], noData=nodata, initValues=nodata, xRes=pixel_size, yRes=-pixel_size, allTouched=False, burnValues=[1], outputSRS="EPSG:"+str(epsg))    
 
 
 def main(yml_fpath):
@@ -27,8 +27,11 @@ def main(yml_fpath):
     #Translate config to dictionary 
     yml_conf = read_yaml(yml_fpath)
     for i in range(len(yml_conf["vector_fn"])):
-        #Run 
-        rasterize(yml_conf["nodata"], yml_conf["vector_fn"][i], yml_conf["pixel_size"])
+        #Run
+        epsg = str(4326)
+        if "epsg" in yml_conf.keys():
+            epsg = yml_conf["epsg"][i]
+        rasterize(yml_conf["nodata"], yml_conf["vector_fn"][i], yml_conf["pixel_size"], epsg)
 
 
 if __name__ == '__main__':
