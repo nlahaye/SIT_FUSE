e7da53a54e2af0edb5371e8e7a26fde73d2d69b2
nlahaye <nlahaye@jpl.nasa.gov>
2024-09-30 17:21:13 +0000

    Adding dilation and erosion capabilities

--- src/sit_fuse/postprocessing/contour_and_fill.py
+++ src/sit_fuse/postprocessing/contour_and_fill.py
@@ -57,7 +57,13 @@ def main(yml_fpath):
         
         if yml_conf["remove_singles"]:
             imgData = imgData - dip.GetSinglePixels(imgData > 0)
- 
+
+  
+        if yml_conf["dilations"] > 0:
+            imgData = cv2.dilate(imgData.copy(), None, iterations=yml_conf["dilations"])
+        if yml_conf["erosions"] > 0:
+            imgData = cv2.erode(imgData.copy(), None, iterations=yml_conf["erosions"])
+
         imgData2 = imgData.copy()
         imgData3 = imgData.copy()
 
