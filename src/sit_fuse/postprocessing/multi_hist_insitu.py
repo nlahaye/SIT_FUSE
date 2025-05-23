94adaf660f45183f1056e899b367c25b778781a2
nlahaye <lahay100@mail.chapman.edu>
2025-05-23 14:11:56 -0700

    Adding flexibility and multi-radius capabilities to multi-histogramming

--- src/sit_fuse/postprocessing/multi_hist_insitu.py
+++ src/sit_fuse/postprocessing/multi_hist_insitu.py
@@ -21,6 +21,29 @@ from skimage.util import view_as_windows
 from copy import deepcopy
 import datetime
 
+
+
+
+
+def build_lookup_dict(labels, lookup):
+
+    for i in range(len(labels)):
+        for j in range(len(labels[i])):
+            if (str(labels[i][j]) not in lookup) or (lookup[str(labels[i][j])] < i):
+                lookup[str(labels[i][j])] = i
+    print(lookup)
+    return lookup
+
+def build_final_list(lookup, final_lst):
+    for key in lookup.keys():
+        final_lst[lookup[key]].append(float(key))
+    for i in range(len(final_lst)):
+        final_lst[i] = sorted(final_lst[i])
+
+ 
+    return final_lst
+    
+
 def main(yml_fpath):
 
     #Translate config to dictionary 
@@ -36,9 +59,61 @@ def main(yml_fpath):
     if 'Karenia' in yml_conf['xl_fname'] or 'karenia' in yml_conf['xl_fname']:
         karenia = True
 
-    insitu_hab_to_multi_hist(yml_conf['xl_fname'], start_date, end_date,
-		yml_conf['clusters_dir'], yml_conf['clusters'], yml_conf['radius_degrees'],
-                yml_conf['ranges'], yml_conf['global_max'], yml_conf['input_file_type'], karenia)
+
+    arr_tmp = [[] for x in range(0,(len( yml_conf['ranges'])-1))] 
+    labels = []
+ 
+    use_key  = 'Total_Phytoplankton'
+    if 'use_key' in yml_conf:
+        use_key = yml_conf['use_key']
+    lookup = {}
+    final_lst = []
+    for i in range(len(yml_conf["ranges"])):
+        final_lst.append([])
+    for i in range(len(yml_conf['radius_degrees'])):
+        d_lower = (i > 0)
+        labels.append(insitu_hab_to_multi_hist(yml_conf['xl_fname'], start_date, end_date,
+    		yml_conf['clusters_dir'], yml_conf['clusters'], yml_conf['radius_degrees'][i],
+                    yml_conf['ranges'], yml_conf['global_max'], yml_conf['input_file_type'], karenia, discard_lower = d_lower, use_key = use_key)) #, lookup = lookup))
+
+        #lookup = build_lookup_dict(labels[-1], lookup)
+
+
+    #final_lst = build_final_list(lookup, final_lst)
+
+
+    arr_tmp = [[] for x in range(0,(len( yml_conf['ranges'])-1))]
+    arr_init = labels[0]
+     
+    for n in range(1, len(labels)):
+        for i in range(len(arr_init)):
+            for j in range(len(labels[n][i])):
+                ind = -1
+                for k in range(len(arr_init)):
+                    if labels[n][i][j] in arr_init[k]:
+                        ind = k
+                if ind < i:
+                    arr_tmp[i].append(labels[n][i][j])
+
+        for i in range(len(arr_init)):
+            for j in range(len(arr_init[i])):
+                ind = -1
+                for k in range(len(arr_init)):
+                    if arr_init[i][j] in arr_tmp[k]:
+                        ind = k  
+                if ind < 0:
+                    arr_tmp[i].append(arr_init[i][j])
+
+        arr_init = deepcopy(arr_tmp)
+        arr_tmp = [[] for x in range(0,(len( yml_conf['ranges'])-1))]
+    
+
+    for i in range(len(arr_init)):
+        arr_init[i] = sorted(arr_init[i])  
+    print(arr_init)
+    #print(labels)
+    #print(final_lst)
+
 
 if __name__ == '__main__':
 
