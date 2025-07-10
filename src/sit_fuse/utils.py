56b1b92e8aacb9a97baa09837a0c08e41c6d852d
Stasya Easley <stasyaeasley@Stasyas-MacBook-Pro.local>
2025-07-10 11:24:19 -0700

    mod utils

--- src/sit_fuse/utils.py
+++ src/sit_fuse/utils.py
@@ -1209,7 +1209,7 @@ def insitu_hab_to_multi_hist(insitu_fname, start_date, end_date, clusters_dir, n
     else:
         print(insitu_df['time'])
         # Format Datetime Stamp
-        insitu_df['Datetime'] = pd.to_datetime(insitu_df['time'])
+        insitu_df['Datetime'] = pd.to_datetime(insitu_df['time'], utc=True)
         insitu_df.set_index('Datetime')
          
         insitu_df.rename(columns={"latitude": "Latitude"}, inplace=True)
