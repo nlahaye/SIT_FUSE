cf3656df0401a0344a1731351ea8c09dcf52775c
Stasya Easley <stasyaeasley@Stasyas-MacBook-Pro.local>
2025-07-15 12:11:24 -0700

    revert zonal hist

--- src/sit_fuse/postprocessing/zonal_histogram.py
+++ src/sit_fuse/postprocessing/zonal_histogram.py
@@ -190,7 +190,7 @@ def main(yml_fpath):
     print(len(clust_gtiffs), len(label_gtiffs[0]))
     for j in range(len(label_gtiffs)):
         for i in range(len(label_gtiffs[0])):
-            zonal_histogram, poly_knns = gen_zonal_histogram(label_gtiffs[i], clust_gtiffs[i], zonal_histogram, poly_knns, zone_ind=j+1)
+            zonal_histogram, poly_knns = gen_zonal_histogram(label_gtiffs[j][i], clust_gtiffs[i], zonal_histogram, poly_knns, zone_ind=j+1)
 
         print(len(zonal_histogram.keys()), len(zonal_histogram[list(zonal_histogram.keys())[0]].keys()))
 
@@ -206,14 +206,14 @@ def main(yml_fpath):
 
         print(zonal_histogram.keys(), len(zonal_histogram[list(zonal_histogram.keys())[0]].keys()))
 
-        print("SAVING", os.path.join(out_dir, out_tag + "_hist_dict.pkl"))
+        print("SAVING", os.path.join(out_dir, out_tag[j] + "_hist_dict.pkl"))
 
-        with open(os.path.join(out_dir, out_tag + "_hist_dict.pkl"), 'wb') as handle:
+        with open(os.path.join(out_dir, out_tag[j] + "_hist_dict.pkl"), 'wb') as handle:
                 dump(zonal_histogram, handle, True, pickle.HIGHEST_PROTOCOL)
 
 
-        print("SAVING", os.path.join(out_dir, out_tag + "_base_cluster_polygon_knn_graphs.pkl"))
-        with open(os.path.join(out_dir, out_tag + "_base_cluster_polygon_knn_graphs.pkl"), 'wb') as handle:
+        print("SAVING", os.path.join(out_dir, out_tag[j] + "_base_cluster_polygon_knn_graphs.pkl"))
+        with open(os.path.join(out_dir, out_tag[j] + "_base_cluster_polygon_knn_graphs.pkl"), 'wb') as handle:
                 dump(poly_knns, handle, True, pickle.HIGHEST_PROTOCOL)
 
         #numpy.save(os.path.join(out_dir, out_tag[j] + "_base_cluster_polygon_knn_graphs.npz"), poly_knns, allow_pickle=True)
