7ff12626177411caa6eda7ed9ceaa944fe3dd7cd
Stasya Easley <stasyaeasley@Mac.lan>
2025-07-28 19:23:31 -0700

    mod configs and zonal hist

--- src/sit_fuse/postprocessing/zonal_histogram.py
+++ src/sit_fuse/postprocessing/zonal_histogram.py
@@ -182,12 +182,12 @@ def main(yml_fpath):
     #Run 
     clust_gtiffs = yml_conf["data"]["clust_gtiffs"]
     label_prefixes = yml_conf["data"]["label_gtiffs"][0]
-    bloom_names = yml_conf["data"]["name"]
+    out_tag = yml_conf["data"]["name"]
     out_dir = yml_conf["output"]["out_dir"]
     out_tags = yml_conf["output"]["class_name"]
 
     label_gtiffs = []
-    for bloom_name in bloom_names:
+    for bloom_name in out_tag:
         full_label_paths = [prefix + bloom_name + ".tif" for prefix in label_prefixes]
         label_gtiffs.append(full_label_paths)
 
