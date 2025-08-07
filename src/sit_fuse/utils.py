14775ca87529755b3cc09d7af9de3abf4651761b
Stasya Easley <stasyaeasley@Stasyas-MacBook-Pro.local>
2025-08-06 20:28:52 -0700

    goes validation

--- src/sit_fuse/utils.py
+++ src/sit_fuse/utils.py
@@ -1249,6 +1249,9 @@ def insitu_hab_to_multi_hist(insitu_fname, start_date, end_date, clusters_dir, n
                 file_ext = file_ext + "total_phytoplankton.tif"
             #clust_fname = os.path.join(os.path.join(clusters_dir, "AQUA_MODIS." + pd.to_datetime(str(date)).strftime("%Y%m%d") + ".L3m." + file_ext))
             clust_fname = os.path.join(os.path.join(clusters_dir, pd.to_datetime(str(date)).strftime("%Y%m%d") +  file_ext))
+            if not os.path.exists(clust_fname):
+                # For GOES
+                clust_fname = os.path.join(clusters_dir, pd.to_datetime(str(date)).strftime("%Y%m%d") + "_total_phytoplankton.tif")
         elif "alexandrium" in input_file_type:
             clust_fname = os.path.join(os.path.join(clusters_dir, pd.to_datetime(str(date)).strftime("%Y%m%d") + "_alexandrium_bloom" + ".tif"))
         elif "pseudo_nitzschia_seriata" in input_file_type:
