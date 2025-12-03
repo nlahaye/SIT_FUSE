1c87ef2d85d6c8cf30d6f8978fa5be313306c25e
Nick Lahaye <nlahaye@dragon.sig.sig-gis.com>
2025-12-03 23:37:07 +0000

    Adding oil palm training pipeline functionality

---
+++ src/sit_fuse/pipelines/training/oil_palm_preprocess_and_train_pipeline.py
@@ -0,0 +1,29 @@
+import os
+import argparse
+
+from sit_fuse.utils import read_yaml
+
+from sit_fuse.pipelines.training.oil_palm_preprocess_and_train_utils import run_data_preprocessing, run_model_training
+
+
+def run_oil_palm_preprocess_and_train_pipeline(yml_conf):
+
+    config_dict = run_data_preprocessing(yml_conf)
+
+    run_model_training(config_dict)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-y", "--yaml", help="YAML file for fusion info.")
+    args = parser.parse_args()
+
+    #Translate config to dictionary 
+    yml_conf = read_yaml(args.yaml)
+
+    run_oil_palm_preprocess_and_train_pipeline(yml_conf)
+
+
+
+
